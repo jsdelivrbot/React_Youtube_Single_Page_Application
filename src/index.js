b8fd9eece1f4566aafa9bb9a3cc476ba253a7149
arukshagrover <arukshagrover@gmail.com>
2017-09-04 14:51:22 -0500

    First commit

---
+++ src/index.js
@@ -0,0 +1,57 @@
+import _ from 'lodash';
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+import YTSearch from 'youtube-api-search';
+
+const API_KEY='AIzaSyAMjI17fHTdQawh9oHGTnd8LLmclj8g6zE';
+
+//Create a new component. This componenet should produce some html
+class App extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state={
+      videos: [],
+      selectedVideo: null
+    };
+    this.videoSearch('surfboards');
+  }
+
+  videoSearch(term){
+    YTSearch({key: API_KEY , term:term}, (videos) => {
+      this.setState({
+        videos,
+        selectedVideo: videos[0]
+      });
+      //this.setState({videos:videos})
+    });
+  }
+
+  forAruksha(term){
+    alert(term);
+  }
+
+  render() {
+    const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
+    return(
+      <div>
+        <SearchBar onSearchTermChange= {videoSearch} />
+        <VideoDetail video={this.state.selectedVideo}/>
+
+        <VideoList
+        onVideoSelect={selectedVideo =>this.setState({selectedVideo})}
+        videos={this.state.videos}/>
+      </div>);
+  }
+
+};
+
+
+
+//Take this component's generated html and put it on the page.(on the DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));
